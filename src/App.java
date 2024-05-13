18c9fb719c3451ae69b44d0fddca594e65d7e62b
Oswaldo Chilin <oswaldochilin49@gmail.com>
2024-05-13 11:19:56 -0600

    if para deterinar los postivos

--- src/App.java
+++ src/App.java
@@ -29,8 +29,15 @@ public class App {
     private static void ejecicio2(){
             byte[] numeros = {-3,4,-5,6,-7,8,9};
 
-            for(int i = 0; i < numeros.length; i++){
+            byte[] positivo =new byte[4];
 
+            for(int i = 0; i <= numeros.length; i++){
+
+                if(numeros[i] != 0 ){
+
+                    positivo[i] = numeros[i];
+                }
+               
             }
     }
 }
