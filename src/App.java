f0ae1f32a3d77ca3274ecac7cfa04bde66919bc0
Oswaldo Chilin <oswaldochilin49@gmail.com>
2024-05-13 11:05:05 -0600

    interfaz de ejercicios

--- src/App.java
+++ src/App.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
+        Scanner scanner = new Scanner(System.in);
+
         
-        
+        System.out.println("ingrese el ejercicio que desea ejecutar 1 o 2");
+        int opcion = scanner.nextInt();
+
+        if(opcion == 1)
+        {
+            ejercicio1();
+        }
+        else if(opcion == 2){
+            ejecicio2();
+        }
+        else
+        {
+            System.out.println("esa opcion no existe");
+        }
+    }
+
+    private static void ejercicio1(){
+
+    }
+
+    private static void ejecicio2(){
+
     }
 }
